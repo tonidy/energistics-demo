deb1280dbab3f6bf7a78072b107799cfa86c8959
Welly Tambunan <if05041@gmail.com>
2018-11-11 22:16:31 +0700

    encapsulating etp client encoding and decoding parameter

--- etp_client.py
+++ etp_client.py
@@ -1,24 +1,138 @@
 import sys
 from base64 import b64encode
+from threading import Thread
+import time
+
 import avro.protocol
-import io, random
+import io
 from avro.io import DatumWriter
 from autobahn.twisted.websocket import WebSocketClientProtocol, WebSocketClientFactory, connectWS
 from twisted.python import log
 from twisted.internet import reactor
 from collections import namedtuple
 
-messageMaps = {}
 
-messageHeaderSchema = avro.schema.Parse(open("Schemas/Energistics/Datatypes/MessageHeader.avsc").read())
+class ETPClient(Thread):
+    def __init__(self, url, username, password):
+        super(ETPClient, self).__init__()
+        log.startLogging(sys.stdout)
 
-# w, h = 100, 100
-protocolsMessage = {}
+        headers = {
+            'Authorization': 'Basic {}'.format(b64encode(b'ilab.user:n@6C5rN!').decode("utf-8"))
+        }
+
+        factory = WebSocketClientFactory("wss://witsmlstudio.pds.software/staging/api/etp", headers=headers,
+                                         protocols=["energistics-tp"])
+        self.factory = factory
+
+        self.factory.protocol = ETPClientProtocol
+
+        self.factory.on_etp_message = self.on_etp_message
+
+    def on_etp_message(self, header, body):
+        print(header)
+        print(body)
+
+    def run(self):
+        reactor.run(installSignalHandlers=0)
+
+    def connect(self):
+        self.daemon = True
+        connectWS(self.factory)
+        self.start()
+
+    def create_request_session_message(self):
+        requestSession = {
+            "applicationName": "",
+            "applicationVersion": "",
+            "requestedProtocols": [
+                {
+                    "protocol": 1,
+                    "protocolVersion": {
+                        "major": 1,
+                        "minor": 1,
+                        "revision": 0,
+                        "patch": 0
+                    },
+                    "role": "producer",
+                    "protocolCapabilities": {}
+                },
+                {
+                    "protocol": 3,
+                    "protocolVersion": {
+                        "major": 1,
+                        "minor": 1,
+                        "revision": 0,
+                        "patch": 0
+                    },
+                    "role": "store",
+                    "protocolCapabilities": {}
+                },
+                {
+                    "protocol": 4,
+                    "protocolVersion": {
+                        "major": 1,
+                        "minor": 1,
+                        "revision": 0,
+                        "patch": 0
+                    },
+                    "role": "store",
+                    "protocolCapabilities": {}
+                },
+                {
+                    "protocol": 5,
+                    "protocolVersion": {
+                        "major": 1,
+                        "minor": 1,
+                        "revision": 0,
+                        "patch": 0
+                    },
+                    "role": "store",
+                    "protocolCapabilities": {}
+                },
+                {
+                    "protocol": 6,
+                    "protocolVersion": {
+                        "major": 1,
+                        "minor": 1,
+                        "revision": 0,
+                        "patch": 0
+                    },
+                    "role": "store",
+                    "protocolCapabilities": {}
+                }
+            ],
+            "supportedObjects": []
+        }
+
+        return requestSession
+
+    def request_session(self):
+        print("request session")
+        self.send_etp_message(self.create_request_session_message(), "Energistics.Protocol.Core.RequestSession")
+
+    def send_etp_message(self, message, messageType):
+        self.factory.send_message(message, messageType)
 
 
-class MyClientProtocol(WebSocketClientProtocol):
-    def onError(self, err):
-        print("error")
+class ETPClientProtocol(WebSocketClientProtocol):
+    def __init__(self):
+        self.messageMaps = {}
+        self.messageHeaderSchema = avro.schema.Parse(open("Schemas/Energistics/Datatypes/MessageHeader.avsc").read())
+        self.protocolsMessage = {}
+
+        schema_path = "etp.avpr"
+
+        protocol = avro.protocol.Parse(open(schema_path).read())
+
+        for type in protocol.types:
+            if "protocol" in type.other_props:
+                protocol_no = int(type.other_props["protocol"])
+                message_type = int(type.other_props["messageType"])
+                if not protocol_no in self.protocolsMessage:
+                    self.protocolsMessage[protocol_no] = {}
+                self.protocolsMessage[protocol_no][message_type] = type
+                self.messageMaps[type.namespace + "." + type.name] = type
 
     def onConnect(self, response):
         print("Connected to Server: {}".format(response.peer))
@@ -26,89 +140,32 @@ class MyClientProtocol(WebSocketClientProtocol):
     def onOpen(self):
         print("WebSocket connection open.")
 
-        requestSession = {
-            "applicationName": "",
-            "applicationVersion": "",
-            "requestedProtocols": [
-                                {
-                                  "protocol": 1,
-                                  "protocolVersion": {
-                                    "major": 1,
-                                    "minor": 1,
-                                    "revision": 0,
-                                    "patch": 0
-                                  },
-                                  "role": "producer",
-                                  "protocolCapabilities": {}
-                                },
-                                {
-                                  "protocol": 3,
-                                  "protocolVersion": {
-                                    "major": 1,
-                                    "minor": 1,
-                                    "revision": 0,
-                                    "patch": 0
-                                  },
-                                  "role": "store",
-                                  "protocolCapabilities": {}
-                                },
-                                {
-                                  "protocol": 4,
-                                  "protocolVersion": {
-                                    "major": 1,
-                                    "minor": 1,
-                                    "revision": 0,
-                                    "patch": 0
-                                  },
-                                  "role": "store",
-                                  "protocolCapabilities": {}
-                                },
-                                {
-                                  "protocol": 5,
-                                  "protocolVersion": {
-                                    "major": 1,
-                                    "minor": 1,
-                                    "revision": 0,
-                                    "patch": 0
-                                  },
-                                  "role": "store",
-                                  "protocolCapabilities": {}
-                                },
-                                {
-                                  "protocol": 6,
-                                  "protocolVersion": {
-                                    "major": 1,
-                                    "minor": 1,
-                                    "revision": 0,
-                                    "patch": 0
-                                  },
-                                  "role": "store",
-                                  "protocolCapabilities": {}
-                                }
-                              ],
-            "supportedObjects": []
-        }
-        self.send(requestSession, "Energistics.Protocol.Core.RequestSession")
+        # self.factory.send_message = self.sendMessage
+        self.factory.send_message = self.send_etp_message
 
-    def send(self, message, messageType):
-        bytes_writer = io.BytesIO()
-        encoder = avro.io.BinaryEncoder(bytes_writer)
-
-        # need to send based on the message type
-        header = {"protocol":0,"messageType":1,"correlationId":0,"messageId":1,"messageFlags":0}
-
-        # write header
-        writer = avro.io.DatumWriter(messageHeaderSchema)
-        writer.write(header, encoder)
-
-        # write body
-        writer = avro.io.DatumWriter(messageMaps[messageType])
-        writer.write(message, encoder)
-
-        self.sendMessage(bytes_writer.getvalue(), True)
+        self.factory.messageMaps = self.messageMaps
+        self.factory.messageHeaderSchema = self.messageHeaderSchema
+        self.protocolsMessage = self.protocolsMessage
 
     def onClose(self, wasClean, code, reason):
         print("WebSocket connection closed: {}".format(reason))
+
+    def send_etp_message(self, message, messageType):
+        bytes_writer = io.BytesIO()
+        encoder = avro.io.BinaryEncoder(bytes_writer)
+
+        # need to send based on the message type -> cache this messageType from dictionary
+        header = {"protocol":0,"messageType":1,"correlationId":0,"messageId":1,"messageFlags":0}
+
+        # write header
+        writer = avro.io.DatumWriter(self.factory.messageHeaderSchema)
+        writer.write(header, encoder)
+
+        # write body
+        writer = avro.io.DatumWriter(self.factory.messageMaps[messageType])
+        writer.write(message, encoder)
+
+        self.sendMessage(bytes_writer.getvalue(), True)
 
     def onMessage(self, payload, isBinary):
         if isBinary:
@@ -123,12 +180,12 @@ class MyClientProtocol(WebSocketClientProtocol):
         decoder = avro.io.BinaryDecoder(bytes_reader)
 
         # read header
-        reader = avro.io.DatumReader(messageHeaderSchema)
+        reader = avro.io.DatumReader(self.messageHeaderSchema)
         header = reader.read(decoder)
 
         message_header = namedtuple("MessageHeader", header.keys())(*header.values())
 
-        reader = avro.io.DatumReader(protocolsMessage[message_header.protocol][message_header.messageType])
+        reader = avro.io.DatumReader(self.protocolsMessage[message_header.protocol][message_header.messageType])
         body = reader.read(decoder)
 
         message_body = namedtuple("MessageBody", body.keys())(*body.values())
@@ -136,35 +193,16 @@ class MyClientProtocol(WebSocketClientProtocol):
         return message_header, message_body
 
     def on_etp_message(self, header, message):
-        print(header)
-        print(message)
+        self.factory.on_etp_message(header, message)
 
 
 if __name__ == '__main__':
-    log.startLogging(sys.stdout)
+    etpClient = ETPClient("url", "username", "password")
 
-    headers = {
-        'Authorization': 'Basic {}'.format(b64encode(b'ilab.user:n@6C5rN!').decode("utf-8"))
-    }
-    factory = WebSocketClientFactory("wss://witsmlstudio.pds.software/staging/api/etp", headers=headers, protocols=["energistics-tp"])
+    etpClient.connect()
 
-    factory.protocol = MyClientProtocol
+    time.sleep(3)
 
-    connectWS(factory)
+    openSession = etpClient.request_session()
 
-    schema_path = "etp.avpr"
-    protocol = avro.protocol.Parse(open(schema_path).read())
-
-    for type in protocol.types:
-        print(type.name)
-        if "protocol" in type.other_props:
-            protocol_no = int(type.other_props["protocol"])
-            message_type = int(type.other_props["messageType"])
-            if not protocol_no in protocolsMessage:
-                protocolsMessage[protocol_no] = {}
-            protocolsMessage[protocol_no][message_type] = type
-            messageMaps[type.namespace + "." + type.name] = type
-
-
-    print(protocolsMessage)
-    reactor.run()
+    time.sleep(5)
